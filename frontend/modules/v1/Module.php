d6b729b8a50276d422d9f019668d993f57436501
kapa <kapa16@inbox.ru>
2019-07-11 10:56:08 +0500

    add API bearer authentication

--- frontend/modules/v1/Module.php
+++ frontend/modules/v1/Module.php
@@ -19,6 +19,6 @@ class Module extends \yii\base\Module
     {
         parent::init();
 
-        // custom initialization code goes here
+        \Yii::$app->user->enableSession = false;
     }
 }
--- frontend/modules/v1/controllers/TaskController.php
+++ frontend/modules/v1/controllers/TaskController.php
@@ -3,9 +3,19 @@
 namespace frontend\modules\v1\controllers;
 
 use tasktracker\entities\task\Tasks;
+use yii\filters\auth\HttpBearerAuth;
 use yii\rest\ActiveController;
 
 class TaskController extends ActiveController
 {
     public $modelClass = Tasks::class;
+
+    public function behaviors()
+    {
+        $behaviors = parent::behaviors();
+        $behaviors['authenticator'] = [
+            'class' => HttpBearerAuth::class,
+        ];
+        return $behaviors;
+    }
 }
